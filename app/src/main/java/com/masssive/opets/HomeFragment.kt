4d53840068b740cea6d990a2ceffd91a8e3d4dd2
hasanyibrohim546@gmail.com <hasanyibrohim546@gmail.com>
2023-11-21 21:25:52 +0700

    home

--- app/src/main/java/com/masssive/opets/HomeFragment.kt
+++ app/src/main/java/com/masssive/opets/HomeFragment.kt
@@ -8,11 +8,15 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.masssive.opets.adapter.ListDiskonAdapter
+import com.masssive.opets.adapter.ListPriviewAdapter
+import com.masssive.opets.model.Preview
 import com.masssive.opets.model.diskon
 
 class HomeFragment : Fragment() {
 
     private lateinit var rvDiskon: RecyclerView
+    private lateinit var rvPriview: RecyclerView
+    private val listt = ArrayList<Preview>()
     private val list = ArrayList<diskon>()
 
     override fun onCreateView(
@@ -26,15 +30,36 @@ class HomeFragment : Fragment() {
         rvDiskon = view.findViewById(R.id.rv_dis)
         rvDiskon.setHasFixedSize(true)
 
+        rvPriview = view.findViewById(R.id.recyclerView)
+        rvPriview.setHasFixedSize(true)
+
         // Populate the list and show RecyclerView
         list.addAll(getListDiskon())
+        listt.addAll(getLisPreview())
         showRecyclerList()
 
         return view
     }
 
+    private fun getLisPreview(): Collection<Preview> {
+        val dataPv = resources.obtainTypedArray(R.array.priv_foto)
+        val tgl = resources.getStringArray(R.array.tgl_priv)
+        val listpriview = ArrayList<Preview>()
+
+        for (i in tgl.indices){
+            val priview = Preview(dataPv.getResourceId(i, -1), " ", tgl[i] )
+            listpriview.add(priview)
+        }
+        dataPv.recycle()
+        return listpriview
+
+    }
+
     private fun showRecyclerList() {
         // Use context instead of 'this' in LinearLayoutManager
+        rvPriview.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        val listPriviewAdapter = ListPriviewAdapter(listt)
+        rvPriview.adapter = listPriviewAdapter
         rvDiskon.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
         val listDiskonAdapter = ListDiskonAdapter(list)
         rvDiskon.adapter = listDiskonAdapter
--- app/src/main/java/com/masssive/opets/model/Preview.kt
+++ app/src/main/java/com/masssive/opets/model/Preview.kt
@@ -5,8 +5,7 @@ import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class Preview(
-    val nameP: String,
+    val photoP: Int,
     val description: String,
-    val tanggal: String,
-    val photoP: Int
+    val tanggal: String
 ):Parcelable
