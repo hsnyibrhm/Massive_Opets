ab3cf06d9482e5b9b358c7c5fe4ed95636b4c98f
hasanyibrohim546@gmail.com <hasanyibrohim546@gmail.com>
2023-11-22 09:52:27 +0700

    fix bug

--- app/src/main/java/com/masssive/opets/LoginActivity.kt
+++ app/src/main/java/com/masssive/opets/LoginActivity.kt
@@ -1,17 +1,29 @@
 package com.masssive.opets
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.view.View.OnClickListener
+import com.masssive.opets.databinding.ActivityLoginBinding
+import com.masssive.opets.databinding.ActivityMainBinding
 
-class LoginActivity : AppCompatActivity(), View.OnClickListener{
+class LoginActivity : AppCompatActivity(){
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupAction()
     }
 
-    override fun onClick(v: View?) {
-        TODO("Not yet implemented")
+    private fun setupAction() {
+        binding.btnLogin.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
