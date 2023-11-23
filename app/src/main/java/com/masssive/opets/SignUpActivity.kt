cd058ab4c0d92e7eb1ec92f74d35e458c7ffe6b0
hasanyibrohim546@gmail.com <hasanyibrohim546@gmail.com>
2023-11-23 16:02:40 +0700

    pembaruan

--- app/src/main/java/com/masssive/opets/SignUpActivity.kt
+++ app/src/main/java/com/masssive/opets/SignUpActivity.kt
@@ -1,18 +1,18 @@
 package com.masssive.opets
 
+import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.view.View.OnClickListener
 import com.masssive.opets.databinding.ActivityLoginBinding
-import com.masssive.opets.databinding.ActivityMainBinding
+import com.masssive.opets.databinding.ActivitySignUpBinding
 
-class LoginActivity : AppCompatActivity(){
-    private lateinit var binding: ActivityLoginBinding
+class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityLoginBinding.inflate(layoutInflater)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         setupAction()
@@ -23,12 +23,5 @@ class LoginActivity : AppCompatActivity(){
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
-
-        binding.tvsignup.setOnClickListener{
-            val intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
-        }
     }
-
-
 }
