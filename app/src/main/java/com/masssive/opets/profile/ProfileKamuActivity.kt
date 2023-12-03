6580b7032fc008b9af7d167bee46cbeb35868c20
hasanyibrohim546@gmail.com <hasanyibrohim546@gmail.com>
2023-12-03 22:25:56 +0700

    fitur tambahan

---
+++ app/src/main/java/com/masssive/opets/profile/ProfileKamuActivity.kt
@@ -0,0 +1,12 @@
+package com.masssive.opets.profile
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masssive.opets.R
+
+class ProfileKamuActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile_kamu)
+    }
+}
