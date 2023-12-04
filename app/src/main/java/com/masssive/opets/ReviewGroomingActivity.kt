a30f9127db5b7e346f3e823bf9bbb7f271211f0a
hasanyibrohim546@gmail.com <hasanyibrohim546@gmail.com>
2023-12-04 10:23:54 +0700

    halaman

---
+++ app/src/main/java/com/masssive/opets/ReviewGroomingActivity.kt
@@ -0,0 +1,11 @@
+package com.masssive.opets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReviewGroomingActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_review_grooming)
+    }
+}
---
+++ app/src/main/java/com/masssive/opets/chat/ChatCatHotelActivity.kt
@@ -0,0 +1,12 @@
+package com.masssive.opets.chat
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.masssive.opets.R
+
+class ChatCatHotelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat_cat_hotel)
+    }
+}
