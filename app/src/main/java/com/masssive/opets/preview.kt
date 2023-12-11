780c06d29f8f2cacc1ac3fff0c6a2f7b0e7b3a2b
Raihanah Luthfiyah Rosanti <raihanahluthfiyahrosanti@gmail.com>
2023-12-11 15:35:12 +0700

    tes

---
+++ app/src/main/java/com/masssive/opets/preview.kt
@@ -0,0 +1,11 @@
+package com.masssive.opets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class preview : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_preview)
+    }
+}
