1a410f969ca1a3538e8563ec8eadc8e485f32348
fatahfkrn <fatahfkrn@gmail.com>
2023-12-19 23:22:25 +0700

    update_terbaru

---
+++ app/src/main/java/com/masssive/opets/detail_bayar_grooming_kutu.kt
@@ -0,0 +1,11 @@
+package com.masssive.opets
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class detail_bayar_grooming_kutu : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_bayar_grooming_kutu)
+    }
+}
