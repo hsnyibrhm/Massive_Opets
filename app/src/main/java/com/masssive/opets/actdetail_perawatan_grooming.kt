46778f1971d8a1032a016f80917d83e7a1d1295a
Raihanah Luthfiyah Rosanti <raihanahluthfiyahrosanti@gmail.com>
2023-12-19 21:13:50 +0700

    s

--- app/src/main/java/com/masssive/opets/actdetail_perawatan_grooming.kt
+++ app/src/main/java/com/masssive/opets/actdetail_perawatan_grooming.kt
@@ -1,11 +1,19 @@
 package com.masssive.opets
 
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class actdetail_perawatan_grooming : AppCompatActivity() {
+
+abstract class actdetail_perawatan_grooming : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_actdetail_perawatan_grooming)
+
+
+
+        }
+
+
     }
-}
